0eac7421f6a2f18ed6e165e0fe79145379bb24ab
mayeDave <olorunmayedavid1234@gmail.com>
2025-01-29 17:30:07 +0100

    Edited email verification

--- frontend/src/pages/auth/EmailVerificationPage.jsx
+++ frontend/src/pages/auth/EmailVerificationPage.jsx
@@ -14,9 +14,9 @@ const EmailVerificationPage = () => {
       >
         <div className="max-w-md w-full pt-4 bg-base-300 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
           <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-base-content to-base-content/45 text-transparent bg-clip-text">
-              Verify Your Email
-            </h2>
+          <h3 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-base-content to-base-content/45 text-transparent bg-clip-text">
+              Verify Your Email, from your inbox or check spam
+            </h3>
             <p className="text-center mb-6">
               Enter the 6-digit code sent to your email address.
             </p>
